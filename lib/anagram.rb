08e50e5899091ac2da04397cf7539e76752dae5b
cynthia <cchelangat9892gmail.com>
2022-10-24 11:20:33 +0300

    commit tree d5562e1491e5a0e549fc72875c61824a6928954c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+ class Anagram 
+    attr_reader :anagram
+
+    def initialize(anagram)
+        @anagram = anagram 
+    end 
+    def match (values)
+        new_str = @anagram.chars.sort.join
+        values.filter {|value| value.chars.sort.join == new_str }
+    end
+  
+end
